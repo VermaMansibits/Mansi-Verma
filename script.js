1409318b26e7b1096d95d401c6e179557bdb2119
VermaMansibits <Vermamansi.bits@gmail.com>
2024-09-09 21:46:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let flickityInstance = null;
+
+document.addEventListener('DOMContentLoaded', () => {
+    const date = document.getElementById('date')
+    const setCurrentYear = () => {
+        const currentYear = new Date().getFullYear()
+        date.textContent = currentYear
+    }
+    setCurrentYear()
+
+    // hamburger menu
+    const hamburger = document.querySelector('.hamburger')
+    const navList = document.querySelector('.nav-list')
+    const socials = document.querySelector('.social-nav')
+    const navLinks = document.querySelectorAll('.nav-links')
+
+    const toggleMobileMenu = () => {
+        hamburger.classList.toggle('open')
+        navList.classList.toggle('open')
+        socials.classList.toggle('open')
+        document.body.classList.toggle('open')
+    }
+
+    navLinks.forEach(link => link.addEventListener('click', toggleMobileMenu))
+    hamburger.addEventListener('click', toggleMobileMenu)
+
+    // initialize aos (library for scroll animation)
+    AOS.init()
+})
+
+
+function hideOverlay() {
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'none';
+
+	// Destroy Flickity instance when hiding overlay
+    if (flickityInstance) {
+        flickityInstance.destroy();
+        flickityInstance = null;
+    }
+}
+
